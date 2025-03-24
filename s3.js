fda983b1a557de6ca7908a6feb9495f50a66de7d
max <max@guavaapps.com>
2025-03-24 18:13:55 +0000

    commit

--- s3.js
+++ s3.js
@@ -1,8 +1,11 @@
 import {GetObjectCommand, HeadObjectCommand, PutObjectCommand, S3Client} from "@aws-sdk/client-s3"
 import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
+import dotenv from "dotenv";
 
-const ACCESS_KEY = "AKIA6ODVAR4YPFS2OR4P"
-const SECRET_ACCESS_KEY = "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"
+dotenv.config()
+
+const ACCESS_KEY = process.env.S3_ACCESS_KEY
+const SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY
 
 const s3Client = new S3Client({
     region: "eu-north-1",
