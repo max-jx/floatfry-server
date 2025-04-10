da2ab3ec6a9eae4ca3e7fe25e585450a6d465c4f
max <max@guavaapps.com>
2025-04-10 02:09:31 +0100

    final

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
+
 app.use(["/data", "/activate"], (req, res, next) => {
     const authorization = req.header("Authorization")
 
